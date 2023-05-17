d9d42f6649bdfd30e4ffce1ca228cd89012850db
Melissa Neves <melissa.neves@sptech.school>
2023-05-17 18:10:07 -0300

    Área do usuario criada

---
+++ Aplicação Web/site/public/js/user.js
@@ -0,0 +1,77 @@
+var historia = `
+<h1 class="titulo_conteudo">História</h1> 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, consequatur ea. Eum, ullam voluptatum rerum, error voluptatibus, vero dolor minus maiores sunt est accusamus suscipit! Eos earum quas voluptate eveniet.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<p>
+`
+var etica_animal = `
+<h1 class="titulo_conteudo">Ética Animal</h1> 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, consequatur ea. Eum, ullam voluptatum rerum, error voluptatibus, vero dolor minus maiores sunt est accusamus suscipit! Eos earum quas voluptate eveniet.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<p>`
+
+var industria = `
+<h1 class="titulo_conteudo">Indústria</h1> 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, consequatur ea. Eum, ullam voluptatum rerum, error voluptatibus, vero dolor minus maiores sunt est accusamus suscipit! Eos earum quas voluptate eveniet.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<p>`
+
+var meio_ambiente = `
+<h1 class="titulo_conteudo">Meio Ambiente</h1> 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, consequatur ea. Eum, ullam voluptatum rerum, error voluptatibus, vero dolor minus maiores sunt est accusamus suscipit! Eos earum quas voluptate eveniet.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<p>`
+
+var saude = `
+<h1 class="titulo_conteudo">Saúde</h1> 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, consequatur ea. Eum, ullam voluptatum rerum, error voluptatibus, vero dolor minus maiores sunt est accusamus suscipit! Eos earum quas voluptate eveniet.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<p>`
+
+var dignidade_humana = `
+<h1 class="titulo_conteudo">Dignidade Humana</h1> 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, consequatur ea. Eum, ullam voluptatum rerum, error voluptatibus, vero dolor minus maiores sunt est accusamus suscipit! Eos earum quas voluptate eveniet.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<p>`
+
+
+var documentarios = `
+<h1 class="titulo_conteudo">Documenários</h1> 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, consequatur ea. Eum, ullam voluptatum rerum, error voluptatibus, vero dolor minus maiores sunt est accusamus suscipit! Eos earum quas voluptate eveniet.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<br><br>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi saepe sunt repellat quo soluta voluptatem ducimus ut numquam omnis non, cumque neque nihil id quia ea corporis deleniti odit fugit.
+<p>`
